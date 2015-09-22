03b499d045b382c6a1811c5df539478de07b4b01
zacharyh <zacharythunt@gmail.com>
2015-09-21 21:28:35 -0700

    looping-break-gets-done

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,12 @@
 
 def levitation_quiz
-	#your code here
+	loop do
+		puts "What is the spell that enacts levitation?"
+		answer = gets
+		break if answer == "Wingardium Leviosa"
+	end
+	puts "You passed the quiz!"
+ #your code here
 end
 
-
+levitation_quiz
